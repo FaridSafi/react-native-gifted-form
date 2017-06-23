3708c0547d7b27f2a440818b2024cd698960d460
Sam West <sambwest@gmail.com>
2017-06-22 22:13:50 -0500

    Move error generation to GiftedManager (Programmatic Submit) (#99)

--- widgets/GooglePlacesWidget.js
+++ widgets/GooglePlacesWidget.js
@@ -7,17 +7,17 @@ var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplet
 
 module.exports = React.createClass({
   mixins: [WidgetMixin],
-  
+
   getDefaultProps() {
     return {
       type: 'GooglePlacesWidget',
     };
   },
-  
+
   render() {
     const everywhere = {description: 'Everywhere', geometry: { location: { lat: 0, lng: 0 } }};
-    
-    
+
+
     return (
       <GooglePlacesAutocomplete
         placeholder='Type a city name'
@@ -29,6 +29,7 @@ module.exports = React.createClass({
           this._onChange({
             name: details.formatted_address,
             placeId: details.place_id,
+            details: details,
             loc: [details.geometry.location.lng, details.geometry.location.lat],
             types: details.types
           });
@@ -46,7 +47,7 @@ module.exports = React.createClass({
             color: '#1faadb',
           },
         }}
-        
+
         currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
         currentLocationLabel="Current location"
         currentLocationAPI='GoogleReverseGeocoding' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
@@ -58,13 +59,13 @@ module.exports = React.createClass({
           rankby: 'distance',
           types: 'food',
         }}
-        
-        
+
+
         filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-        
+
         // predefinedPlaces={[everywhere]}
-        
-        
+
+
         {...this.props} // @todo test sans (need for 'name')
       />
     );
